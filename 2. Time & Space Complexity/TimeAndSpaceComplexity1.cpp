99f02a699e52fcffc752775801c7113e03dfee58
Naman Pandey <109365773+Namanlko@users.noreply.github.com>
2023-04-23 21:27:27 +0530

    2. Time & Space Complexity

---
+++ 2. Time & Space Complexity/TimeAndSpaceComplexity1.cpp
@@ -0,0 +1,21 @@
+// 1) Find the time complexity of the following code.
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    int N;
+    cin>>N;
+
+    for(int i=0; i<N; i++){
+        for(int j=0; j<N; j++){
+            cout<<(i+j)<<" ";
+        }
+        cout<<endl;
+    }
+
+    // Time Complexity of Code is O(N*N).
+
+    return 0;
+}
---
+++ 2. Time & Space Complexity/TimeAndSpaceComplexity3.cpp
@@ -0,0 +1,16 @@
+// 3) Code to understand Space Complexity.
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    int a,b;
+
+    cin>>a;         // Input Space
+    cin>>b;         // Input Space
+    int c = a+b;    // Auxillary Space
+    cout<<c<<endl;
+
+    return 0;
+}
